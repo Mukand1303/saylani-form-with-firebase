2f43579f11ec69d765866032cec524f3d538cb57
Mukand Kirshana <mukandkirshana1606@gmail.com>
2023-12-20 10:34:54 +0500

    almost done

---
+++ config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBX67VR2H_R9PNIy6NQfCbvit73nW3Ci3M",
+  authDomain: "saylani-form-960e3.firebaseapp.com",
+  projectId: "saylani-form-960e3",
+  storageBucket: "saylani-form-960e3.appspot.com",
+  messagingSenderId: "514247873762",
+  appId: "1:514247873762:web:8fc047ecb1363dae663abb",
+  measurementId: "G-8MPLGZ2EQ0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
